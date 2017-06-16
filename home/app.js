7090792863fb2a608419bb138c8aa1f51dce6bdb
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-06-15 20:24:57 -0400

    things be broken

--- home/app.js
+++ home/app.js
@@ -10,7 +10,7 @@ angular.module('App', ['ui.router'])
       //  // messages: function (FetchService) {
       //  //  return FetchService.getMessages();
       // }
-    }
+    //}
   })
   .state({
     name: 'login',
