d1bcd4f8e045699c86c8a0549bee70f2e1943321
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-06-22 12:10:10 -0400

    updates to map

--- home/controllers/homeController.js
+++ home/controllers/homeController.js
@@ -1,31 +1,27 @@
 angular.module('App').
-controller('HomeController', ['$scope',  '$rootScope', 'userService', 'mappingTools',
-function ($scope, $rootScope, userService, mappingTools) {
-  $scope.tiles = {name: 'Mapbox Streets',  
-                  url: '//api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2RhbW9uIiwiYSI6InFVV1VLMFUifQ.2Zx0T9w01EK6E-v76-z85Q'
-          }
-  angular.extend($scope, {
-    currentLoc: {
-            lat:40.76624155398039,
-            lon: -73.94433975219727,
-            zoom:12
-          },
-          tiles: {name: 'Mapbox Streets',  
-                  url: '//api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2RhbW9uIiwiYSI6InFVV1VLMFUifQ.2Zx0T9w01EK6E-v76-z85Q'
-          }
-  });
-    userService //authentication
-      .authenticate()
-      .then(function (user) { $scope.user = user });
+controller('HomeController', ['$scope', '$rootScope', 'userService', 'mappingTools', 'Data',
+function ($scope, $rootScope, userService, mappingTools, Data) {
+  
+  userService //authentication
+    .authenticate()
+    .then(function (user) { $scope.user = user });
 
-    mappingTools //get current loc from browser
-      .getCurrentPosition().then((position) => {
-        console.log(position.coords);
-        $scope.currentLoc = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude,
-                  zoom: 12
-              }
-          });
+  $scope.tiles = mappingTools.defaultTile; //map set up
+  $scope.defaults = { scrollWheelZoom: false
+                      } //map set up
+  $scope.currentLoc = mappingTools.defaultLoc; //map set up
+
+
+  mappingTools //get current loc from browser
+    .getCurrentPosition().then((position) => {
+      $scope.currentLoc = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                zoom: 12
+            }
+        });
+
+  //$scope.geojson = mappingTools.eventToGeoJson(Data)
+   
 }]);
             
--- home/services/mappingTools.js
+++ home/services/mappingTools.js
@@ -1,4 +1,4 @@
-angular.module('App').factory('mappingTools', ['$q', '$window', function ($q, $window) {
+angular.module('App').factory('mappingTools', ['$q', '$window', '$http', function ($q, $window, $http) {
   function getCurrentPosition() {
     var deferred = $q.defer();
 
@@ -15,7 +15,59 @@ angular.module('App').factory('mappingTools', ['$q', '$window', function ($q, $w
         return deferred.promise;
     }
 
+  function getEvents() {
+    return $http.get('/events', {contentType: 'application/json'})
+    .then(function (response) {
+      console.log('Get Successful: ', response);  
+      return response.data;
+    })
+    .catch(function (response) {
+      console.error('Get Failed', response);
+    });
+  }
+
+  function eventToGeoJson(data) {
+    var geoJson = {
+      "type": "FeatureCollection",
+      "features": []
+    }
+    var geoJsonPoint =  {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": []
+          },
+          "properties": {}
+        }
+
+    data.forEach(function(point) {
+      var newpoint = geoJsonPoint;
+      newpoint.geometry.coordinates.push(point.location.lat, point.location.lng)
+      for (let property in point) {
+        if (property !== 'location') {
+          newpoint.properties[property] = point[property]
+        }
+      }
+      geoJson.features.push(newpoint)
+    })
+    
+    return JSON.stringify(geoJson);
+
+  }    
+
+  defaultLoc = {
+                  lat: 40.7475170623211,
+                  lng: -73.95343780517578,
+                  zoom: 12
+                }
+
+  defaultTile = {'//api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoid2RhbW9uIiwiYSI6InFVV1VLMFUifQ.2Zx0T9w01EK6E-v76-z85Q'}
+
     return {
-      getCurrentPosition : getCurrentPosition
+      getCurrentPosition : getCurrentPosition,
+      getEvents: getEvents,
+      eventToGeoJson: eventToGeoJson,
+      defaultLoc: defaultLoc,
+      defaultTile: defaultTile,
     }
 }]);
