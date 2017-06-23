b632b8525fa3c3fc23d5ff507b328e27f8a1b0e0
Tianyi Zhao <tianyi@Tianyis-MacBook-Air.local>
2017-06-23 11:58:57 -0400

    update

--- models/event.js
+++ models/event.js
@@ -5,18 +5,13 @@ var eventSchema = new mongoose.Schema({
   description: String,
   host: String,
   type: String,
-<<<<<<< HEAD
+  // location: { type: String, coordinates: [Number] },
   location: { address: String, lng: Number, lat: Number },
-=======
-  location: { type: String, coordinates: [Number] },
->>>>>>> adjusted event db for timestamp
   desiredParticipants: Number,
   time: String,
   price: Number,
   confirmedParticipants: String,
   potentialParticipants: String
-  },   
-{ timestamps: { createdAt: 'created_at' } });
+});
 
 module.exports = mongoose.model('Event', eventSchema);
-
