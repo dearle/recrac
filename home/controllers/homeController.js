5769294d4411aad123e0f70684fd007016c74975
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-06-24 10:57:15 -0400

    better card rendering

--- home/controllers/homeController.js
+++ home/controllers/homeController.js
@@ -49,4 +49,8 @@ function ($scope, $rootScope, $state, userService, searchServices, mappingTools,
     $state.go("app.event", {eventId: eventId});
   };
 
+
+  $scope.openEventDetails = function(eventId) {
+    $state.go("app.event", {eventId: eventId});
+  };
 }])
