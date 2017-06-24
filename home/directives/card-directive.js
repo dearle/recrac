4643446f877eef2e43afcdb39eff8b264526d17a
William  Damon <wgdamon@Williams-MacBook-Pro.local>
2017-06-24 18:05:34 -0400

    dynamic styling for home page

--- home/directives/card-directive.js
+++ home/directives/card-directive.js
@@ -2,8 +2,28 @@ angular.module('App')
 .directive('cardDirective', ['$http', function($http) {
   return {
     restrict: 'E',
-    controller: function ($scope) {},
+    controller: function ($scope) {
+      // $scope.styles = {
+      //   'Job': {
+      //     color: '#fff',
+      //     background-image: "url(https:'./pic/job.jpg'), bottom right 15%, no-repeat #46B6AC"
+      //   },
+      //   "Class": {
+      //     color: '#fff',
+      //     background: "url(https:'./pic/class.jpg'), bottom right 15%, no-repeat #46B6AC"
+      //   },
+      //   "Food": {
+      //     color: '#fff',
+      //     background: "url(https:'./pic/food.jpg') bottom right 15% no-repeat #46B6AC"
+      //   },
+      //   "Party": {
+      //     color: '#fff',
+      //     background: "url(https:'./pic/party.jpg') bottom right 15% no-repeat #46B6AC"
+      //   }
+
+      // }
+
+    },
     templateUrl: '../templates/card.html'
   };
 }]);
-
