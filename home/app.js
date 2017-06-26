a50059133046d60892da6c13acffb20cac5dc500
David Earle <davidearle@Davids-MacBook-Pro.local>
2017-06-26 01:39:50 -0400

    Tae's commit + messages working

--- home/app.js
+++ home/app.js
@@ -55,13 +55,35 @@ angular.module('App', ['ui.router', 'ngMaterial', 'ngAria', 'ngAnimate', 'ngAuto
     url: "/events/:eventId",
     params: {event: null},
     templateUrl: './templates/app.event.html',
-    controller: function ($scope, $stateParams, userService, $state) {
+    controller: function ($scope, $stateParams, userService, $state, mappingTools, $http) {
+      
       $scope.id = $stateParams.eventId;
-      $scope.event = $state.params.event
+      //$scope.event = $state.params.event
+      $scope.event = {};
+      $scope.messages = [];
+      
+      mappingTools.getEvent($scope.id).then(function(data) {
+        $scope.event = data;
+      })
+
+      mappingTools.getMessages($scope.id).then(function(data) {
+        $scope.messages = data;
+      })
+      
       $scope.save = function() {
         mappingTools.saveEvent($scope.event, $scope.id).then(function() {
           alert("TEST SAVE");
         })
+      }
+      
+      $scope.saveMessage = function() {
+        $http.post("/message", {event: $scope.id, user:"", text: $scope.message.text}, {contentType: 'application/json'})
+        .then(function (response) {
+          console.log('Post Successful: ', response);  
+        })
+        .catch(function (err) {
+          console.error('Post Failed: ', err);
+        });
       }
     }
   })
